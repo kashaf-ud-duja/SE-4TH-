40990100ff9bc91ea60a44cdfcbfe995be415e08
kashaf-ud-duja <80465225+kashaf-ud-duja@users.noreply.github.com>
2021-06-10 16:06:31 +0500

    Add files via upload
    non primitive datatype

---
+++ pass by value or pass by refrence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pass_by_value_or_pass_by_refrence
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a:{0},b:{1}", a, b));
+
+            var array1 = new int[3] { 1, 2, 3 };
+            array2[0] = 0;
+                Console.WriteLine(string.Format("array1[0]:{0},array2[0]:{1}",array1[0],Array2[0]));
+        }
+    }
+}
