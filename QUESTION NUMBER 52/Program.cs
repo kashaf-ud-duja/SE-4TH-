c9a541daa10804be87024cf358550b040e09fa17
kashaf-ud-duja <80465225+kashaf-ud-duja@users.noreply.github.com>
2021-06-11 13:17:47 +0500

    arithmetic operators

---
+++ QUESTION NUMBER 52/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace QUESTION_NUMBER_52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((1+8/2+((1*4)+(5*4))/4));
+            // (a);
+            Console.WriteLine(((1 + 1 + 1 + 1) / 2 + (1 + 1 + 1) / 3));
+            // (b);
+            Console.WriteLine((5 * 5 + 5 / 5 + 6));
+            // (c);
+            Console.WriteLine((((3 + 4) + (4 * 7)) / 5));
+            // (d);
+            Console.WriteLine(((3 * 6 * 7 * 2) + 12 / 2));
+            // (e);
+            Console.WriteLine(5 - 3 * 4 / (6 - 1));
+            // (f);
+            Console.WriteLine((8 * 4 * 2 + 6) / 2 + 4);
+        }
+    }
+}
