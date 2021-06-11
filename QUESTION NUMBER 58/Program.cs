91845d193723f9f7ede00032fa544017219770fc
kashaf-ud-duja <80465225+kashaf-ud-duja@users.noreply.github.com>
2021-06-11 13:19:44 +0500

    arithmetic operators

---
+++ QUESTION NUMBER 58/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QUESTION_NUMBER_58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 2;
+            b = 3;
+            c = 5;
+
+            Console.WriteLine("the result is"+ ++a);
+            Console.WriteLine("the result is" + a*b*c);
+            Console.WriteLine("the result is" + a++);
+            Console.WriteLine("the result is" + b + a);
+
+        }
+    }
+}
