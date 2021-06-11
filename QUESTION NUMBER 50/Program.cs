eac2f0c2dd455e0e4378374021ad377861d55c22
kashaf-ud-duja <80465225+kashaf-ud-duja@users.noreply.github.com>
2021-06-11 13:15:31 +0500

    airthmatic operators

---
+++ QUESTION NUMBER 50/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace QUESTION_NUMBER_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+
+           Console.WriteLine("the result is"+(x*x + 5*x-4));
+
+            int z = 1;
+            int y = 2;
+
+            Console.WriteLine("the result is" + ((z +y )*z));
+
+            int t = 1;
+            Console.WriteLine("the result is" + 1 / (t * t + t +3));
+
+            int a = 0, b = 5, c = 4;
+            a = (3 * b) / (5 - c);
+            Console.WriteLine("the result is" + (x));
+
+
+
+
+        }
+    }
+}
